6a76087d26278e6abb9e8663a53dc5a3e3f3bb3d
Tommy <tomiandrada2000@gmail.com>
2026-02-16 01:26:27 -0300

    Primeros pasos en microservicios. Prox. fix ckeab y re-install root

---
+++ microservices/customer-microservices/src/test/java/com/tommy/microservices/customer_microservices/CustomerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.microservices.customer_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
